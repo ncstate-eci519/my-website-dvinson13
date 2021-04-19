4b2c2c748b6bf5e09c2c5dedff471c62063ec5a8
dvinson13 <71404356+dvinson13@users.noreply.github.com>
2021-04-18 21:04:51 -0400

    JAVA
    fixed unit 9 and added submit trigger to email

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-    $("#toggler").click(function(){
-      $(".form-group").toggle();
-    });
+  $("#toggler").click(function(){
+    $(".form-group").toggle();
+    $("form").submit(function(){
+      event.preventDefault();
+      var webform = $.post("https://maker.ifttt.com/trigger/webform/with/key/cIMCLpyvyaP1uljIYwyhUN"), JSON.stringify({"value1": $("email").val(), "value2": $("problem").val()});
+    })
   });
+});
